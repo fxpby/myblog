f640835f25468e782d63270d7bf358fd7d0351ca
fxpby <fxpby@outlook.com>
2021-12-14 02:02:27 +0800

    feat: fix footer style

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -42,25 +42,10 @@
         <a
           target="_blank"
           href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=12011602000961"
-          style="
-            display: inline-block;
-            text-decoration: none;
-            height: 20px;
-            line-height: 20px;
-          "
-          ><img src="/myblog/beian.png" style="float: left" />
-          <p
-            style="
-              float: left;
-              height: 20px;
-              line-height: 20px;
-              margin: 0px 0px 0px 5px;
-              color: #939393;
-            "
-          >
+          style="display: inline-block;text-decoration: none;height: 20px;line-height: 20px;"><img src="/myblog/beian.png" style="float: left" />
+          <p style="float: left;height: 20px;line-height: 20px;margin: 0px 0px 0px 5px;color: #939393;">
             津公网安备 12011602000961号
-          </p></a
-        >
+          </p></a>
       </div>
     </div>
   </main>
