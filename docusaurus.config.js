3b633e1552ded7019fb531721548ce59cc088a84
fxpby <fxpby@outlook.com>
2022-09-01 11:37:13 +0800

    feat: config navbar hide on scroll

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,6 +61,7 @@ const config = {
     ({
       navbar: {
         title: 'fxpby和olu是好朋友',
+        hideOnScroll: true,
         logo: {
           alt: '',
           src: process.env.DEPLOY_SERVER === 'deploygithub' ? '/myblog/img/headImg.jpg' : '/img/headImg.jpg',
