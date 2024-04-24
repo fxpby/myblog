d30deabb26c8ca2d32cf1cbf66672efb829bb0f2
fxpby <fxpby@outlook.com>
2024-04-25 00:58:06 +0800

    chore: update catalogue

--- sidebars.js
+++ sidebars.js
@@ -98,10 +98,7 @@ const sidebars = {
     'vue/vue2/vue2-init-process',
     'vue/vue2/vue2-reactivity',
   ],
-  React: [
-    'react/react-re-renders',
-    'react/react-elements-children-parents'
-  ],
+  React: ['react/react-re-renders', 'react/react-elements-children-parents'],
   'front-end-engineering': [
     'front-end-engineering/travis-ci-for-vuepress',
     'front-end-engineering/github-action-for-vuepress',
@@ -127,6 +124,12 @@ const sidebars = {
       type: 'category',
       label: 'nginx',
       items: ['nginx/nginx-cors', 'nginx/nginx-cache/nginx-cache-directive'],
+    },
+    {
+      type: 'category',
+      label: 'webpack',
+      collapsed: false,
+      items: ['front-end-engineering/webpack/loader'],
     },
   ],
   browser: [
