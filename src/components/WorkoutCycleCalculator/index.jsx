8380a46b1830e6e8a80a17e9e789772797a3bc94
fxpby <fxpby@outlook.com>
2024-07-13 00:52:39 +0800

    feat: fix props

--- src/components/WorkoutCycleCalculator/index.jsx
+++ src/components/WorkoutCycleCalculator/index.jsx
@@ -54,120 +54,117 @@ const targetLoadRatioList = {
   },
 };
 
+const handleChange = useCallback((callback, val) => {
+  callback(val);
+}, []);
+
+const BaseCalculator = memo(({rep, oneRM, cycleName} = {}) => {
+  console.log('BaseCalculator');
+  const tempColumns = [...columns];
+  const repFitMap = {
+    10: 0.75 * oneRM,
+    12: 0.67 * oneRM,
+  };
+  tempColumns.forEach((col) => {
+    if (col.id === 'rep') {
+      col.items = new Array(5).fill(`${rep}RM ${repFitMap[rep].toFixed(2)}kg`);
+    }
+    if (col.id === 'targetLoad') {
+      col.items = targetLoadRatioList[cycleName][rep].map(
+        (ratio) =>
+          `${(repFitMap[rep] * ratio).toFixed(2)}kg (${(ratio * 100).toFixed(
+            2,
+          )}%)`,
+      );
+    }
+    if (col.id === 'count') {
+      col.items = new Array(5).fill(rep);
+    }
+  });
+
+  const displayTableData = rawList.map((raw, index) => {
+    const targetColItem = (id) =>
+      tempColumns.find((x) => x.id === id).items[index];
+    return {
+      name: targetColItem('name'),
+      rep: targetColItem('rep'),
+      targetLoad: targetColItem('targetLoad'),
+      count: targetColItem('count'),
+      group: targetColItem('group'),
+    };
+  });
+
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            {tempColumns.map((col) => (
+              <Th>{col.label}</Th>
+            ))}
+          </Tr>
+        </Thead>
+        <Tbody>
+          {displayTableData.map((row) => (
+            <Tr>
+              <Td>{row.name}</Td>
+              <Td>{row.rep}</Td>
+              <Td>{row.targetLoad}</Td>
+              <Td>{row.count}</Td>
+              <Td>{row.group}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+});
+
+const CycleCard = memo(({inputChange, oneRM, cycleName, title} = {}) => {
+  return (
+    <div className={s.cycleCardWrapper}>
+      <Tag size="lg">{title}</Tag>
+      <div>
+        请输入 1RM 的重量(kg)
+        <NumberInput
+          defaultValue={0}
+          value={oneRM}
+          min={0}
+          max={500}
+          onChange={(valueAsString, valueAsNumber) =>
+            handleChange(inputChange, valueAsNumber)
+          }>
+          <NumberInputField />
+          <NumberInputStepper>
+            <NumberIncrementStepper />
+            <NumberDecrementStepper />
+          </NumberInputStepper>
+        </NumberInput>
+      </div>
+      <div>
+        <div>12 RM 小周期 ① (W 代表周)</div>
+        <BaseCalculator rep={12} oneRM={oneRM} cycleName={cycleName} />
+      </div>
+      <div>
+        <div>10 RM 小周期 ② (W 代表周)</div>
+        <BaseCalculator rep={10} oneRM={oneRM} cycleName={cycleName} />
+      </div>
+    </div>
+  );
+});
+
 const WorkoutCycleCalculator = (props) => {
   const {cycleName, title} = props;
-  const [oneRMNormalPrepare, setOneRMNormalPrepare] = useState(0);
-  const [oneRMB, setOneRMB] = useState(0);
-  const [oneRMD, setOneRMD] = useState(0);
-
-  const BaseCalculator = memo(({rep, oneRM, cycleName} = {}) => {
-    console.log('BaseCalculator');
-    const tempColumns = [...columns];
-    const repFitMap = {
-      10: 0.75 * oneRM,
-      12: 0.67 * oneRM,
-    };
-    tempColumns.forEach((col) => {
-      if (col.id === 'rep') {
-        col.items = new Array(5).fill(
-          `${rep}RM ${repFitMap[rep].toFixed(2)}kg`,
-        );
-      }
-      if (col.id === 'targetLoad') {
-        col.items = targetLoadRatioList[cycleName][rep].map(
-          (ratio) =>
-            `${(repFitMap[rep] * ratio).toFixed(2)}kg (${(ratio * 100).toFixed(
-              2,
-            )}%)`,
-        );
-      }
-      if (col.id === 'count') {
-        col.items = new Array(5).fill(rep);
-      }
-    });
-
-    const displayTableData = rawList.map((raw, index) => {
-      const targetColItem = (id) =>
-        tempColumns.find((x) => x.id === id).items[index];
-      return {
-        name: targetColItem('name'),
-        rep: targetColItem('rep'),
-        targetLoad: targetColItem('targetLoad'),
-        count: targetColItem('count'),
-        group: targetColItem('group'),
-      };
-    });
-
-    return (
-      <TableContainer>
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              {tempColumns.map((col) => (
-                <Th>{col.label}</Th>
-              ))}
-            </Tr>
-          </Thead>
-          <Tbody>
-            {displayTableData.map((row) => (
-              <Tr>
-                <Td>{row.name}</Td>
-                <Td>{row.rep}</Td>
-                <Td>{row.targetLoad}</Td>
-                <Td>{row.count}</Td>
-                <Td>{row.group}</Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
-    );
-  });
-
-  const handleChange = useCallback((callback, val) => {
-    callback(val);
-  }, []);
-
-  const CycleCard = memo(({inputChange, oneRM, cycleName} = {}) => {
-    return (
-      <div className={s.cycleCardWrapper}>
-        <Tag size="lg">{title}</Tag>
-        <div>
-          请输入 1RM 的重量(kg)
-          <NumberInput
-            defaultValue={0}
-            value={oneRM}
-            min={0}
-            max={500}
-            onChange={(valueAsString, valueAsNumber) =>
-              handleChange(inputChange, valueAsNumber)
-            }>
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </div>
-        <div>
-          <div>12 RM 小周期 ① (W 代表周)</div>
-          <BaseCalculator rep={12} oneRM={oneRM} cycleName={cycleName} />
-        </div>
-        <div>
-          <div>10 RM 小周期 ② (W 代表周)</div>
-          <BaseCalculator rep={10} oneRM={oneRM} cycleName={cycleName} />
-        </div>
-      </div>
-    );
-  });
+  const [oneRM, setOneRM] = useState(0);
 
   return (
     <ChakraProvider resetCSS={false} disableGlobalStyle={true}>
       <div className={s.workoutCycleCalculatorWrapper}>
         <CycleCard
-          inputChange={setOneRMNormalPrepare}
-          oneRM={oneRMNormalPrepare}
+          inputChange={setOneRM}
+          oneRM={oneRM}
           cycleName={cycleName}
+          title={title}
         />
       </div>
     </ChakraProvider>
