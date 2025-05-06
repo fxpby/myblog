828240ffcaaa4911167d0a3b9ac52dc8ee8e0ae5
fxpby <fxpby@outlook.com>
2025-05-06 21:13:31 +0800

    refactor: add post hooks logic

---
+++ src/components/PostBlogs/usePostHooks.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const usePostHooks = ({ blogPostId } = {}) => {
+  const [inputValue, setInputValue] = useState("");
+  const [message, setMessage] = useState("请输入暗号");
+  const [isShow, setIsShow] = useState(false);
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleVerify = () => {
+    if (inputValue.trim() === "seagull" || inputValue.trim() === "海鸥") {
+      setMessage("大厨太棒啦，校验通过！✌️");
+      setIsShow(true);
+    } else {
+      setMessage("嘤嘤校验失败，请重新输入暗号。🤧");
+      setInputValue("");
+      setIsShow(false);
+    }
+  };
+
+  const contentElement = (
+    <div>
+      <input type="text" value={inputValue} onChange={handleChange} />
+      <button onClick={handleVerify}>校验</button>
+      <p>{message}</p>
+      {isShow ? (
+        <div dangerouslySetInnerHTML={{ __html: blogPostId }}></div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+
+  return {
+    contentElement,
+  };
+};
+export default usePostHooks;
