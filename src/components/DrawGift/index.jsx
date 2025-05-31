f80392a2611416e1c11aba7919a420aa8daacc34
fxpby <fxpby@outlook.com>
2025-05-31 12:10:47 +0800

    feat: 抽盲盒组件

---
+++ src/components/DrawGift/index.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from "react";
+import { ChakraProvider, Button } from "@chakra-ui/react";
+import GrawItem from "./item";
+import s from "./styles.module.css";
+
+export default function DrawGift(props = {}) {
+  const metaList = [
+    {
+      name: "原味白米粽",
+      src: "https://fxpby.oss-cn-beijing.aliyuncs.com/blogImg/seagull/zz-nuomi.jpg",
+      step: 0,
+    },
+    {
+      name: "蜜枣甜粽",
+      src: "https://fxpby.oss-cn-beijing.aliyuncs.com/blogImg/seagull/zz-mizao.jpg",
+      step: 0,
+    },
+    {
+      name: "豆沙红豆粽",
+      src: "https://fxpby.oss-cn-beijing.aliyuncs.com/blogImg/seagull/zz-dousha.jpg",
+      step: 0,
+    },
+    {
+      name: "蛋黄鲜肉粽",
+      src: "https://fxpby.oss-cn-beijing.aliyuncs.com/blogImg/seagull/zz-danhuang.jpg",
+      step: 0,
+    },
+    {
+      name: "紫薯糯米粽",
+      src: "https://fxpby.oss-cn-beijing.aliyuncs.com/blogImg/seagull/zz-zishu.jpg",
+      step: 0,
+    },
+    {
+      name: "蜂蜜粽",
+      src: "https://fxpby.oss-cn-beijing.aliyuncs.com/blogImg/seagull/zz-fengmi.jpg",
+      step: 0,
+    },
+    {
+      name: "八宝粽",
+      src: "https://fxpby.oss-cn-beijing.aliyuncs.com/blogImg/seagull/zz-babao.jpg",
+      step: 0,
+    },
+    {
+      name: "惊喜海鸥粽",
+      src: "https://fxpby.oss-cn-beijing.aliyuncs.com/blogImg/seagull/zz-seagull.jpg",
+      step: 0,
+    },
+  ];
+
+  const [drawList, setDrawList] = useState([]);
+  const [drawCount, setDrawCount] = useState(3);
+
+  useEffect(() => {
+    setDrawList(metaList.sort((x) => Math.random() - 0.5));
+  }, []);
+
+  const init = () => {
+    setDrawList(metaList.sort((x) => Math.random() - 0.5));
+    setDrawCount(3);
+  };
+
+  return (
+    <ChakraProvider resetCSS={false} disableGlobalStyle={true}>
+      <p className={s.title}>
+        🍃端午安康，去俩小可爱那里偷了几个可香可香的粽子回来，大厨来抓个盲粽品尝一下吧~
+        剩余次数：
+        {drawCount}
+      </p>
+
+      <div className={s.box}>
+        {drawList.map((listItem) => (
+          <GrawItem
+            listItem={listItem}
+            setDrawCount={setDrawCount}
+            setDrawList={setDrawList}
+            drawList={drawList}
+            drawCount={drawCount}
+          />
+        ))}
+      </div>
+      <div className={s.again}>
+        {drawCount === 0 ? (
+          <div>
+            <div>
+              (注：惊喜海鸥粽是之前码头餐厅大厨包的，不是海鸥馅，唔，可爱海鸥不能吃🤤)
+            </div>
+            <Button onClick={() => init()}>这次不算再来一次!</Button>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </ChakraProvider>
+  );
+}
---
+++ src/components/DrawGift/item.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from "react";
+import {
+  ChakraProvider,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  ListItem,
+  UnorderedList,
+  Box,
+} from "@chakra-ui/react";
+import GrawItem from "./item";
+import s from "./styles.module.css";
+
+export default function DrawGift(props = {}) {
+  const { listItem, setDrawCount, setDrawList, drawCount, drawList } = props;
+  console.log("props: ", props);
+
+  const itemClick = () => {
+    console.log(22);
+    if (drawCount <= 0 || listItem.step === 2) {
+      return;
+    }
+    setDrawCount(drawCount - 1);
+    setDrawList((prev) => {
+      return drawList.map((x) => {
+        if (x.name === listItem.name) {
+          x.step = 1;
+        }
+        return x;
+      });
+    });
+
+    if (drawCount === 1) {
+      setDrawList((prev) => {
+        return drawList.map((x) => {
+          if (x.step !== 1) {
+            x.step = 2;
+          }
+          return x;
+        });
+      });
+    }
+  };
+
+  const getBackClass = () => {
+    let result = ``;
+    if (listItem.step === 1) {
+      result = `${s.back} ${s.show}`;
+    } else if (listItem.step === 0) {
+      result = `${s.back}`;
+    } else {
+      result = `${s.back} ${s.result}`;
+    }
+    return result;
+  };
+
+  return (
+    <ChakraProvider resetCSS={false} disableGlobalStyle={true}>
+      <div className={s.item} onClick={itemClick}>
+        {/* 前面 */}
+        <div className={listItem.step > 0 ? `${s.front} ${s.hide}` : s.front}>
+          {listItem.step > 0 ? (
+            <div className={s.imgWrap}>
+              <img src={listItem.src} alt="" />
+              <div>{listItem.name}</div>
+            </div>
+          ) : (
+            "抓幸运粽"
+          )}
+        </div>
+        {/* 后面 */}
+        <div className={getBackClass()}>
+          {listItem.step > 0 ? (
+            <div className={s.imgWrap}>
+              <img src={listItem.src} alt="" />
+              <div>{listItem.name}</div>
+            </div>
+          ) : (
+            "抓幸运粽"
+          )}
+        </div>
+      </div>
+    </ChakraProvider>
+  );
+}
