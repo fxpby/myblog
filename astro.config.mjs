df7f8588158b15275a09d9fa3a99b40452d7c5bb
fxpby <fxpby@outlook.com>
2026-01-24 15:17:44 +0800

    feat: 重构优化

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,9 +64,20 @@ export default defineConfig({
                 {
                   label: "TypeScript",
                   items: [
-                    { label: "基础", autogenerate: { directory: "coding/typescript/basic" } },
-                    { label: "进阶", autogenerate: { directory: "coding/typescript/advance" } },
-                    { label: "类型体操", autogenerate: { directory: "coding/typescript/type-challenge" } },
+                    {
+                      label: "基础",
+                      autogenerate: { directory: "coding/typescript/basic" },
+                    },
+                    {
+                      label: "进阶",
+                      autogenerate: { directory: "coding/typescript/advance" },
+                    },
+                    {
+                      label: "类型体操",
+                      autogenerate: {
+                        directory: "coding/typescript/type-challenge",
+                      },
+                    },
                   ],
                 },
               ],
@@ -91,17 +102,51 @@ export default defineConfig({
             {
               label: "前端工程化",
               items: [
-                { slug: "coding/front-end-engineering/build-frontend-project-standardization" },
+                {
+                  slug: "coding/front-end-engineering/build-frontend-project-standardization",
+                },
                 { slug: "coding/front-end-engineering/babel" },
                 { slug: "coding/front-end-engineering/jsdoc" },
-                { slug: "coding/front-end-engineering/github-action-for-vuepress" },
+                {
+                  slug: "coding/front-end-engineering/github-action-for-vuepress",
+                },
                 { slug: "coding/front-end-engineering/travis-ci-for-vuepress" },
-                { label: "发布策略", autogenerate: { directory: "coding/front-end-engineering/release-strategy" } },
-                { label: "部署", autogenerate: { directory: "coding/front-end-engineering/deploy" } },
-                { label: "CI/CD", autogenerate: { directory: "coding/front-end-engineering/cicd" } },
-                { label: "Docker", autogenerate: { directory: "coding/front-end-engineering/docker" } },
-                { label: "npm", autogenerate: { directory: "coding/front-end-engineering/npm" } },
-                { label: "Webpack", autogenerate: { directory: "coding/front-end-engineering/webpack" } },
+                {
+                  label: "发布策略",
+                  autogenerate: {
+                    directory: "coding/front-end-engineering/release-strategy",
+                  },
+                },
+                {
+                  label: "部署",
+                  autogenerate: {
+                    directory: "coding/front-end-engineering/deploy",
+                  },
+                },
+                {
+                  label: "CI/CD",
+                  autogenerate: {
+                    directory: "coding/front-end-engineering/cicd",
+                  },
+                },
+                {
+                  label: "Docker",
+                  autogenerate: {
+                    directory: "coding/front-end-engineering/docker",
+                  },
+                },
+                {
+                  label: "npm",
+                  autogenerate: {
+                    directory: "coding/front-end-engineering/npm",
+                  },
+                },
+                {
+                  label: "Webpack",
+                  autogenerate: {
+                    directory: "coding/front-end-engineering/webpack",
+                  },
+                },
               ],
             },
             { label: "浏览器", autogenerate: { directory: "coding/browser" } },
@@ -112,38 +157,110 @@ export default defineConfig({
             {
               label: "计算机网络",
               items: [
-                { label: "缓存", autogenerate: { directory: "coding/network/cache" } },
-                { label: "HTTP", autogenerate: { directory: "coding/network/http" } },
+                {
+                  label: "缓存",
+                  autogenerate: { directory: "coding/network/cache" },
+                },
+                {
+                  label: "HTTP",
+                  autogenerate: { directory: "coding/network/http" },
+                },
               ],
             },
             {
               label: "数据结构",
               items: [
-                { label: "时间复杂度", autogenerate: { directory: "coding/data-structures/time-complexity" } },
-                { label: "空间复杂度", autogenerate: { directory: "coding/data-structures/space-complexity" } },
-                { label: "数组", autogenerate: { directory: "coding/data-structures/array" } },
-                { label: "栈", autogenerate: { directory: "coding/data-structures/stack" } },
-                { label: "队列", autogenerate: { directory: "coding/data-structures/queue" } },
-                { label: "链表", autogenerate: { directory: "coding/data-structures/linkedList" } },
-                { label: "集合", autogenerate: { directory: "coding/data-structures/set" } },
-                { label: "字典", autogenerate: { directory: "coding/data-structures/dictionary" } },
-                { label: "哈希表", autogenerate: { directory: "coding/data-structures/hashtable" } },
-                { label: "树", autogenerate: { directory: "coding/data-structures/tree" } },
-                { label: "图", autogenerate: { directory: "coding/data-structures/graph" } },
-                { label: "堆", autogenerate: { directory: "coding/data-structures/heap" } },
+                {
+                  label: "时间复杂度",
+                  autogenerate: {
+                    directory: "coding/data-structures/time-complexity",
+                  },
+                },
+                {
+                  label: "空间复杂度",
+                  autogenerate: {
+                    directory: "coding/data-structures/space-complexity",
+                  },
+                },
+                {
+                  label: "数组",
+                  autogenerate: { directory: "coding/data-structures/array" },
+                },
+                {
+                  label: "栈",
+                  autogenerate: { directory: "coding/data-structures/stack" },
+                },
+                {
+                  label: "队列",
+                  autogenerate: { directory: "coding/data-structures/queue" },
+                },
+                {
+                  label: "链表",
+                  autogenerate: {
+                    directory: "coding/data-structures/linkedList",
+                  },
+                },
+                {
+                  label: "集合",
+                  autogenerate: { directory: "coding/data-structures/set" },
+                },
+                {
+                  label: "字典",
+                  autogenerate: {
+                    directory: "coding/data-structures/dictionary",
+                  },
+                },
+                {
+                  label: "哈希表",
+                  autogenerate: {
+                    directory: "coding/data-structures/hashtable",
+                  },
+                },
+                {
+                  label: "树",
+                  autogenerate: { directory: "coding/data-structures/tree" },
+                },
+                {
+                  label: "图",
+                  autogenerate: { directory: "coding/data-structures/graph" },
+                },
+                {
+                  label: "堆",
+                  autogenerate: { directory: "coding/data-structures/heap" },
+                },
               ],
             },
             {
               label: "算法",
               items: [
-                { label: "排序算法", autogenerate: { directory: "coding/algorithm/sorting-algorithm" } },
-                { label: "搜索算法", autogenerate: { directory: "coding/algorithm/search-algorithm" } },
-                { label: "位运算", autogenerate: { directory: "coding/algorithm/bitwise-operation" } },
-                { label: "随机算法", autogenerate: { directory: "coding/algorithm/random-algorithm" } },
+                {
+                  label: "排序算法",
+                  autogenerate: {
+                    directory: "coding/algorithm/sorting-algorithm",
+                  },
+                },
+                {
+                  label: "搜索算法",
+                  autogenerate: {
+                    directory: "coding/algorithm/search-algorithm",
+                  },
+                },
+                {
+                  label: "位运算",
+                  autogenerate: {
+                    directory: "coding/algorithm/bitwise-operation",
+                  },
+                },
+                {
+                  label: "随机算法",
+                  autogenerate: {
+                    directory: "coding/algorithm/random-algorithm",
+                  },
+                },
               ],
             },
             { label: "力扣", autogenerate: { directory: "coding/leetcode" } },
-            { label: "数据库", autogenerate: { directory: "coding/database" } },
+            // { label: "数据库", autogenerate: { directory: "coding/database" } },
           ],
         },
         {
@@ -174,7 +291,9 @@ export default defineConfig({
           items: [
             {
               label: "功能营养素",
-              autogenerate: { directory: "nutrition-science/functional-nutrients" },
+              autogenerate: {
+                directory: "nutrition-science/functional-nutrients",
+              },
             },
             {
               label: "维生素",
@@ -200,7 +319,9 @@ export default defineConfig({
           items: [
             {
               label: "内分泌系统与激素",
-              autogenerate: { directory: "functional-medicine/endocrine-system-and-hormones" },
+              autogenerate: {
+                directory: "functional-medicine/endocrine-system-and-hormones",
+              },
             },
           ],
         },
@@ -226,7 +347,12 @@ export default defineConfig({
           label: "摄影",
           collapsed: true,
           items: [
-            { label: "《iPhone手机摄影指南》", autogenerate: { directory: "photography/iPhone-photography-guide" } },
+            {
+              label: "《iPhone手机摄影指南》",
+              autogenerate: {
+                directory: "photography/iPhone-photography-guide",
+              },
+            },
           ],
         },
         {
@@ -243,7 +369,10 @@ export default defineConfig({
           label: "工具",
           collapsed: true,
           items: [
-            { label: "Obsidian", autogenerate: { directory: "tools/obsidian" } },
+            {
+              label: "Obsidian",
+              autogenerate: { directory: "tools/obsidian" },
+            },
           ],
         },
       ],
@@ -261,10 +390,14 @@ export default defineConfig({
     resolve: {
       alias: {
         "@babel/runtime/helpers/extends": "@babel/runtime/helpers/esm/extends",
-        "@babel/runtime/helpers/objectWithoutPropertiesLoose": "@babel/runtime/helpers/esm/objectWithoutPropertiesLoose",
-        "@babel/runtime/helpers/inheritsLoose": "@babel/runtime/helpers/esm/inheritsLoose",
-        "@babel/runtime/helpers/assertThisInitialized": "@babel/runtime/helpers/esm/assertThisInitialized",
-        "@babel/runtime/helpers/defineProperty": "@babel/runtime/helpers/esm/defineProperty",
+        "@babel/runtime/helpers/objectWithoutPropertiesLoose":
+          "@babel/runtime/helpers/esm/objectWithoutPropertiesLoose",
+        "@babel/runtime/helpers/inheritsLoose":
+          "@babel/runtime/helpers/esm/inheritsLoose",
+        "@babel/runtime/helpers/assertThisInitialized":
+          "@babel/runtime/helpers/esm/assertThisInitialized",
+        "@babel/runtime/helpers/defineProperty":
+          "@babel/runtime/helpers/esm/defineProperty",
       },
     },
     ssr: {
